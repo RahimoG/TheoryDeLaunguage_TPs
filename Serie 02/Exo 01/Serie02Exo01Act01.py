8d9fa289f146663538891de59ce726b3268e6c59
RahimoG <65134079+RahimoG@users.noreply.github.com>
2023-03-22 15:18:31 +0100

    Update Serie02Exo01Act01.py

--- Serie 02/Exo 01/Serie02Exo01Act01.py
+++ Serie 02/Exo 01/Serie02Exo01Act01.py
@@ -1,22 +1,20 @@
 def flatten_word(*w_tuple):
     my_string = ""
     for tuple in w_tuple:
-        for i in range(tuple[1]):
-            my_string += tuple[0]
+        my_string += tuple[0] * tuple[1]
     return my_string
 
 
-print("flaten word normal : "+flatten_word(("a",2), ("b", 4), ("a", 2), ("b", 6), ("c", 1)))
+print("flaten word normal : " + flatten_word(("a",2), ("b", 4), ("a", 2), ("b", 6), ("c", 1)))
 
 
 def flatten_word1(*w_tuple):
     L = [
-        tuple[0]
+        tuple[0] * tuple[1]
         for tuple in w_tuple
-            for i in range(tuple[1])
     ]
     return "".join(L)
-print("flaten word with list comprehention : "+flatten_word(("a",2), ("b", 4), ("a", 2), ("b", 6), ("c", 1)))
+print("flaten word with list comprehention : "+flatten_word1(("a",2), ("b", 4), ("a", 2), ("b", 6), ("c", 1)))
 
 
 
